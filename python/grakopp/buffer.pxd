4748a06f000597a820a69650d086f9d09cba967b
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2014-08-01 01:48:34 +0200

    Actually add cython buffer interface.

---
+++ python/grakopp/buffer.pxd
@@ -0,0 +1,43 @@
+# python/grakopp/cpp/buffer.pyx - Grako++ Python bindings -*- coding: utf-8 -*-
+# Copyright (C) 2014 semantics Kommunikationsmanagement GmbH
+# Written by Marcus Brinkmann <m.brinkmann@semantics.de>
+#
+# This file is part of Grako++.  Grako++ is free software; you can
+# redistribute it and/or modify it under the terms of the 2-clause
+# BSD license, see file LICENSE.TXT.
+
+from libcpp.string cimport string
+from libcpp cimport bool
+
+cdef extern from "<memory>" namespace "std":
+    cdef cppclass shared_ptr[T]:
+        T& operator*()
+
+    # Oh well.  Seems that returning templatized types doesn't work.
+    cdef cppclass BufferPtr
+    cdef BufferPtr make_shared[Buffer]()
+
+cdef extern from "grakopp/buffer.hpp":
+    ctypedef shared_ptr[Buffer] BufferPtr
+
+    cdef cppclass Buffer:
+        size_t _pos
+        void from_string(const string& text) nogil
+        void from_file(const string& filename) nogil
+        size_t len() nogil
+        bool atend() nogil
+        bool ateol() nogil
+        char current() nogil
+        char at(size_t pos) nogil
+        char peek(size_t off) nogil
+        char next() nogil
+        void go_to(size_t pos) nogil
+        void move(size_t off) nogil
+        void next_token() nogil
+        size_t skip_to(char ch) nogil
+        size_t skip_past(char ch) nogil
+        size_t skip_to_eol() nogil
+        bool is_space() nogil
+        bool is_name_char(size_t pos) nogil
+        bool match(string token) nogil
+        # boost::optional<std::string> matchre(std::string pattern) nogil
---
+++ python/grakopp/buffer.pyx
@@ -0,0 +1,58 @@
+# python/grakopp/cpp/ast.pyx - Grako++ Python bindings -*- coding: utf-8 -*-
+# Copyright (C) 2014 semantics Kommunikationsmanagement GmbH
+# Written by Marcus Brinkmann <m.brinkmann@semantics.de>
+#
+# This file is part of Grako++.  Grako++ is free software; you can
+# redistribute it and/or modify it under the terms of the 2-clause
+# BSD license, see file LICENSE.TXT.
+
+from cython.operator cimport dereference as deref
+
+cdef class PyBuffer(object):
+    cdef BufferPtr buffer
+    def __cinit__(self):
+        self.buffer = make_shared[Buffer]()
+    def __dealloc__(self):
+        pass
+    property pos:
+        def __get__(self):
+            return deref(self.buffer)._pos
+        def __set__(self, p):
+            deref(self.buffer).go_to(pos)
+    def from_string(self, str):
+        deref(self.buffer).from_string(str)
+    def from_file(self, str):
+        deref(self.buffer).from_file(str)
+    def len(self):
+        return deref(self.buffer).len()
+    def atend(self):
+        return deref(self.buffer).atend()
+    def ateol(self):
+        return deref(self.buffer).ateol()
+    def current(self):
+        return deref(self.buffer).current()
+    def at(self, pos):
+        return deref(self.buffer).at(pos)
+    def at(self, off):
+        return deref(self.buffer).peek(off)
+    def next(self):
+        return deref(self.buffer).next()
+    def go_to(self, pos):
+        deref(self.buffer).go_to(pos)
+    def move(self, off):
+        deref(self.buffer).move(off)
+    def next_token(self):
+        deref(self.buffer).next_token()
+    def skip_to(self, ch):
+        return deref(self.buffer).skip_to(ch)
+    def skip_past(self, ch):
+        return deref(self.buffer).skip_past(ch)
+    def skip_to_eol(self):
+        return deref(self.buffer).skip_to_eol()
+    def is_space(self):
+        return deref(self.buffer).is_space()
+    def is_name_char(self, pos):
+        return deref(self.buffer).is_name_char(pos)
+    def match(self, token):
+        return deref(self.buffer).match(token)
+    # boost::optional<std::string> matchre(std::string pattern) nogil
